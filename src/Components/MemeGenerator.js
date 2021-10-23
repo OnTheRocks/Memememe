e71e91f9367496b21088b3e997a286263b60db05
OnTheRocks <nathan_huber@yahoo.com>
2021-10-22 22:12:54 -0500

    Added new components.

---
+++ src/Components/MemeGenerator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MemeGenerator extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from MemeGenerator</h1>
+      </div>
+    )
+  }
+}
