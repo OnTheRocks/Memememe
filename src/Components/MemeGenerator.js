7bfbe33fa17e9910e0de4fec3db9e13199d0bcb6
OnTheRocks <nathan_huber@yahoo.com>
2021-10-22 22:36:46 -0500

    Setup header and inilized MemeGenerator State.

--- src/Components/MemeGenerator.js
+++ src/Components/MemeGenerator.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export default class MemeGenerator extends Component {
+  constructor() {
+    super()
+    this.state = {
+      topText: "",
+      bottomText: "",
+      rndImage: "http://i.imgflip.com/1bij.jpg"
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Hello from MemeGenerator</h1>
+        <img src={this.state.rndImage}/>
       </div>
     )
   }
